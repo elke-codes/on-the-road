a3374f213f858188ec2d5879623a88b572fd5091
Elke Dick <elke.codes@gmail.com>
2022-01-20 09:12:17 -0800

    add deleted homepage back in

---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import "./HomePage.scss";
+
+import React from "react";
+import Register from "../../components/Register/Register";
+import Map from "../../components/Map/Map";
+
+const HomePage = () => {
+	return (
+		<main>
+			{/* <button onClick={handleOpenRegister}>Register! </button> */}
+
+			{/* <Map /> */}
+			<Register />
+		</main>
+	);
+};
+
+export default HomePage;
