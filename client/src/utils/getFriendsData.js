cd09085183d6ea613d24facf74125e0bc18bd2bc
Elke Dick <elke.codes@gmail.com>
2022-01-23 10:48:44 -0800

    refactor getfriendsdata to exclude loggedinuser

--- client/src/utils/getFriendsData.js
+++ client/src/utils/getFriendsData.js
@@ -3,9 +3,15 @@ import axios from "axios";
 export const getFriendsData = async (loggedInUser) => {
 	// const userName = loggedInUser.userName;
 	console.log("userName", loggedInUser);
-	const result = await axios.get(`http://localhost:8000/users/`);
-	console.log("getfriendsData", result);
-	return result.data.filter((user) => {
-		return user.userName !== loggedInUser;
-	});
+	const result = await axios
+		.get(`http://localhost:8000/users/`)
+		.then((result) => {
+			const allButLoggedInUser = result.data.filter((user) => {
+				return user.userName !== loggedInUser.userName;
+			});
+			// console.log("allbutloggedinuser", allButLoggedInUser);
+			return allButLoggedInUser;
+		});
+	console.log("result", result);
+	return result;
 };
