31a4d9bd9170a3d0403b135e2542e2ed78c8e60a
Elke Dick <elke.codes@gmail.com>
2022-02-04 14:30:25 -0800

    rerender minimap to selected friends location

--- client/src/components/FriendCardCopy/FriendCardCopy.jsx
+++ client/src/components/FriendCardCopy/FriendCardCopy.jsx
@@ -2,23 +2,60 @@
 
 import "./FriendCardCopy.scss";
 import { distanceBetweenCoordinates } from "../../utils/location/distanceBetweenCoordinates";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "../Avatar/Avatar";
 import DividerLine from "../DividerLine/DividerLine";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
 const FriendCardCopy = ({ loggedInUser, selectedFriend }) => {
-	console.log("friendcardcopy selectedfriend", selectedFriend.locations[0]);
+	console.log(
+		"friendcardcopy selectedfriend",
+		selectedFriend.locations[0].lat
+	);
 	console.log("friendcardcopy loggedInUser", loggedInUser);
+	const selectedFriendLat = selectedFriend.locations[0].lat;
+	const selectedFriendLng = selectedFriend.locations[0].lng;
 
+	const [center, setCenter] = useState([
+		selectedFriend.locations[0].lat,
+		selectedFriend.locations[0].lng
+	]);
+
+	useEffect(async () => {
+		if (!selectedFriend) {
+			return;
+		}
+		setCenter([selectedFriendLat, selectedFriendLng]);
+	}, [selectedFriend]);
+
+	// https://stackoverflow.com/questions/64665827/react-leaflet-center-attribute-does-not-change-when-the-center-state-changes
+	const zoom = 10;
+	const ChangeView = ({ center, zoom }) => {
+		const map = useMap();
+		map.setView(center, zoom);
+		return null;
+	};
+
+	console.log(center);
 	return (
 		<div class="card text-center shadow-2xl">
-			<iframe
+			<MapContainer
 				className="card__mini-map"
+				center={center}
+				zoom={3}
+				scrollWheelZoom={true}
 				id="inlineFrameExample"
 				title="Inline Frame Example"
 				width="300"
-				height="200"
-				src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"></iframe>
+				height="200">
+				<ChangeView center={center} zoom={zoom} />
+
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+			</MapContainer>
+			{/* )} */}
 
 			<div class="card-body">
 				<div className="card-title__wrapper">
